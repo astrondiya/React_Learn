446fa791a2571dafcf34652ee6b8a78039a7f0f0
astrondiya <158036991+astrondiya@users.noreply.github.com>
2024-02-02 09:29:02 +0000

    basic react app created

---
+++ 1_Js_refresher.js
@@ -0,0 +1,23 @@
+// variables
+
+let a = 45
+console.log(a);
+const b = 6;
+
+// Data types
+// Numbers, strings, objects, boolean, undefined, null
+
+// functions
+function hello(){
+    console.log("Hello Diya!");
+}
+
+// Objects
+let obj1 = {
+    x:1,
+    y:2,
+    func : function myFunc(number){
+        console.log('The numbers is' + number);
+    }
+}
+console.log(obj1.func);
